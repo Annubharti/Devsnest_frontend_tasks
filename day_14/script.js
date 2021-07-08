92ffaff2d3317d2b90a88824435d8310209aa4b4
Annubharti <annu17@navgurukul.org>
2021-07-08 19:05:39 +0530

    added

---
+++ day_14/script.js
@@ -0,0 +1,28 @@
+// function offFunction() {
+//     alert("your browser is working online")
+
+// }
+
+// function myfunction() {
+//     alert("This is printed")
+
+
+
+// }
+
+// function beforeprint() {
+//     alert("You are about to print this document")
+// }
+
+// function myFunction() {
+//     return "Write something clever here...";
+// // }
+
+// function myfunction() {
+//     var x = document.getElementById("fname");
+//     x.value = x.value.toUpperCase();
+// // }
+// function MyFunction() {
+//     document.getElementById("demo").innerHTML = "IshaBharti";
+
+// }
